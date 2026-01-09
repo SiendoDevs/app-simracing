7189239b0e34cd769c0b81eeaa30e718f2d3d935
CatoSanchez <catriel.sl@gmail.com>
2026-01-09 09:33:01 -0300

    refactor(layout): remove google tag manager scripts and noscript tags
    The Google Tag Manager integration was removed as it's no longer needed for tracking purposes. This cleanup simplifies the layout component and removes unused dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import { ThemeClerkProvider } from "@/components/ThemeClerkProvider";
 import AuthButtons from "@/components/AuthButtons";
 import SteamIdGate from "@/components/SteamIdGate";
 import { Toaster } from "sonner";
-import Script from "next/script";
  
 import { Anek_Gurmukhi, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -39,16 +38,6 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PNQ29PJN" 
-            height="0" 
-            width="0" 
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <ThemeClerkProvider>
             <header className="border-b bg-background">
@@ -72,17 +61,6 @@ export default function RootLayout({
             <SteamIdGate />
           </ThemeClerkProvider>
         </ThemeProvider>
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PNQ29PJN');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
       </body>
     </html>
   );
